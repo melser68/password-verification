176fd12af0fa856152811ddf6ccc0d2181e8a657
SERHII MELNYK <melser68@i.ua>
2023-08-28 10:37:08 +0300

    Create password_verification_task.py

---
+++ password_verification_task.py
@@ -0,0 +1,34 @@
+import re
+import string
+
+pass_patern_lower = re.compile(fr'[{string.ascii_lowercase}+]')
+pass_pattern_upper = re.compile(fr'[{string.ascii_uppercase}+]')
+pass_pattern_punctuation = re.compile(fr'[{string.punctuation}+]')
+
+
+def check_password(password:str) ->str:
+    if len(password) < 8:
+        return 'Password is short (minimal 8 characters)'
+    else:
+        result = []
+        if not pass_patern_lower.findall(password):
+            result.append('No symbols lower')
+        if not pass_pattern_upper.findall(password):
+            result.append('No symbols upper')
+        if not pass_pattern_punctuation.findall(password):
+            result.append('No special symbols')
+        if len(result) == 0:
+            return f'Password: "{password}" - is correct !'
+        else:
+            return f'!!!Password "{password}" is not correct!!!\n={" ; ".join(result)}='
+        
+
+if __name__ == '__main__':
+    print('===Rules for password - min 9 characters, chars upper, chars lower, special symbols===')
+    while True:
+        password = input('Input your password for checking:\n>>>>  ')
+        print(check_password(password))
+        choice = input('Repeat ? (yes/no)\n>>>>  ')
+        if choice.lower() == 'no':
+            break
+
